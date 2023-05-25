5e6bb547be3318a140422a5c14d489ef736c47dc
Captainlearyo <clayoleary@gmail.com>
2023-05-25 16:55:44 -0500

    create board

---
+++ Connect_4/lib/board.rb
@@ -0,0 +1,38 @@
+class Board
+ #attr_reader :
+ #attr__accessor :
+    def initialize
+       @level_1 = [".",".",".",".","."]
+       @level_2 = [".",".",".",".","."]
+       @level_3 = [".",".",".",".","."]
+       @level_4 = [".",".",".",".","."]
+       @level_5 = [".",".",".",".","."]
+      @matrix = [ @level_1,
+                  @level_2,
+                  @level_3,
+                  @level_4,
+                  @level_5]
+
+    end  
+
+   def add (x)
+
+    
+    end
+
+  def print
+
+    p @level_1
+    p @level_2
+    p @level_3
+    p @level_4
+    p @level_5
+
+    end
+
+end
+
+
+
+                  
+                  

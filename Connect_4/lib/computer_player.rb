f584e62e88e7a5c177ebd619ccbbaddc9fed51bb
Parker Boeing <parker.boeing@gmail.com>
2023-05-30 12:14:08 -0600

    Refactor: make computer player class function with random selections

--- Connect_4/lib/computer_player.rb
+++ Connect_4/lib/computer_player.rb
@@ -1,15 +1,14 @@
-require "./lib/insertable"
+
 
 class ComputerPlayer
-include Insertable
   def initialize
   end
 
   def make_move(board)
     columns = ["A", "B", "C", "D", "E", "F", "G"]
     column = columns.sample
-    if valid_column?(column)
-      board.insert_o(column)
+    if board.insert_o(column)
+      true
     else
       make_move(board)
     end

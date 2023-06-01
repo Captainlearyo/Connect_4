308bee3f18371d5b96647fd7c0c4cfb32c29cd7c
Parker Boeing <parker.boeing@gmail.com>
2023-06-01 02:36:47 -0600

    Feat: add levels and speech for quest game play

--- Connect_4/lib/computer_player.rb
+++ Connect_4/lib/computer_player.rb
@@ -1,8 +1,5 @@
-
-
 class ComputerPlayer
-  def initialize
-
+  def initialize(level = 3)
     @index_chart = {
       0 => 'A',
       1 => 'B',
@@ -12,23 +9,30 @@ class ComputerPlayer
       5 => 'F',
       6 => 'G'
     }
+    @level = level
   end
 
-
-  
-  
-
   def logic(board)
-    if logic_check_rows(board)
-      return  board.insert_o(logic_check_rows(board))
-    elsif logic_check_columns(board)
-      return  board.insert_o(logic_check_columns(board))
-    elsif logic_check_rows_add_o(board)
-      return  board.insert_o(logic_check_rows_add_o(board))
-    elsif logic_check_columns_add_o(board)
-      return  board.insert_o(logic_check_columns_add_o(board))
+    if @level == 3
+      if logic_check_rows(board)
+        return  board.insert_o(logic_check_rows(board))
+      elsif logic_check_columns(board)
+        return  board.insert_o(logic_check_columns(board))
+      elsif logic_check_rows_add_o(board)
+        return  board.insert_o(logic_check_rows_add_o(board))
+      elsif logic_check_columns_add_o(board)
+        return  board.insert_o(logic_check_columns_add_o(board))
+      else
+        return random_move(board)
+      end
+    elsif @level == 2
+      if logic_check_rows(board)
+        return  board.insert_o(logic_check_rows(board))
+      else
+        return random_move(board)
+      end
     else
-      return random_move(board)
+      random_move(board)
     end
   end
 
@@ -41,15 +45,6 @@ class ComputerPlayer
       random_move(board)
     end
   end
-
-
-  # def random_move(board)
-  #   columns = ["A", "B", "C", "D", "E", "F", "G"]
-  #   column = columns.sample 
-  #   if board.valid_column?(column)
-  #     return column
-  #   end
-  # end
 
   def logic_check_rows(board)
     m = board.matrix
@@ -80,13 +75,11 @@ class ComputerPlayer
     col_F = [m[0][5], m[1][5], m[2][5],m[3][5],m[4][5], m[5][5]].join
     col_G = [m[0][6], m[1][6], m[2][6],m[3][6],m[4][6], m[5][6]].join  
     col_matrix = [col_A, col_B, col_C, col_D, col_E, col_F, col_G]
-
     col_matrix.each_with_index do |col, i|
        col 
       if col.include?(".XX")
          @index_chart[i]
        return @index_chart[i]
-      
       end
     end
     return false 
@@ -101,7 +94,6 @@ class ComputerPlayer
     diag_5 = [m[5][2], m[4][3], m[3][4],m[2][5], m[1][6]] 
     diag_6 = [m[5][3], m[4][4], m[3][5],m[2][6]]
     diag_arr = [diag_1, diag_2, diag_3, diag_4, diag_5, diag_6]
-
     diag_arr.each do |diag|
       diag.each_with_index do |val ,i|
         prev = diag[i - 1]
@@ -111,24 +103,19 @@ class ComputerPlayer
           #p "XX...."
           return  @index_chart[i + 1]
         end
-
       end
     end
- 
   end
 
   def logic_check_diagonal_2(board)
-    m = board.matrix
-    
+    m = board.matrix   
     diag_7 = [m[0][3], m[1][4], m[2][5],m[3][6]]
     diag_8 = [m[0][2], m[1][3], m[2][4],m[3][5], m[4][6]]
     diag_9 = [m[0][0], m[1][1], m[2][2],m[3][3], m[4][4], m[5][5]]
     diag_10 = [m[0][1], m[1][2], m[2][3],m[3][4],m[4][5], m[5][6]]
     diag_11 = [m[1][0], m[2][1], m[3][2],m[4][3],m[5][4]]
     diag_12 = [m[2][0], m[3][1], m[4][2],m[5][3]]
-
     diag_arr =[diag_7, diag_8, diag_9, diag_10, diag_11, diag_12]
-
     diag_arr.each do |diag|
       diag.each_with_index do |val ,i|
         prev = diag[i - 1]
@@ -138,10 +125,8 @@ class ComputerPlayer
           #p "XX...."
           return  @index_chart[i + 1]
         end
-
       end
     end
-
   end
 
   def logic_check_rows_add_o(board)
@@ -171,18 +156,40 @@ class ComputerPlayer
     col_F = [m[0][5], m[1][5], m[2][5],m[3][5],m[4][5], m[5][5]].join
     col_G = [m[0][6], m[1][6], m[2][6],m[3][6],m[4][6], m[5][6]].join  
     col_matrix = [col_A, col_B, col_C, col_D, col_E, col_F, col_G]
-
     col_matrix.each_with_index do |col, i|
        col 
       if col.include?(".OO")
          @index_chart[i]
-       return @index_chart[i]
-      
+       return @index_chart[i]  
       end
     end
     return false 
   end
 
+  def level_1_talk
+    retorts = [
+      "Wow, nice move, very original.",
+      "Must've taken a lot of brain power to come up with that one."
+    ]
+    retort = retorts.sample
+    puts "Computer Player: '#{retort}'"
+  end
 
+  def level_2_talk
+    retorts = [
+      "You have the charisma of a goldfish.",
+      "I've seen better plays from peewee football teams."
+    ]
+    retort = retorts.sample
+    puts "Computer Player: '#{retort}'"
+  end
 
+  def level_3_talk
+    retorts = [
+      "I AM THE PARAGON OF INTELLIGENCE!!",
+      "YOU ARE MERE PEANUTS TO MY BINARY BRAIN!!"
+    ]
+    retort = retorts.sample
+    puts "Computer Player: '#{retort}'"
+  end
 end
